f6acd7ec8fa385230c8c8e27ba6a1b11c286e8ea
Rocio Valentin <rociovcl.10@gmail.com>
2026-03-23 01:29:12 -0500

    chore: not parsing getRedisConfig

--- src/helper/redis_connection.ts
+++ src/helper/redis_connection.ts
@@ -1,13 +1,15 @@
 export const getRedisConfig = () => {
   // si existe REDIS_URL producción
   if (process.env.REDIS_URL) {
-    const redisUrl = new URL(process.env.REDIS_URL!);
+    // const redisUrl = new URL(process.env.REDIS_URL!);
+    console.log('Using Redis URL:', process.env.REDIS_URL);
 
     return {
-      host: redisUrl.hostname,
-      port: Number(redisUrl.port),
-      ...(redisUrl.username && { username: redisUrl.username }),
-      ...(redisUrl.password && { password: redisUrl.password }),
+        url: process.env.REDIS_URL,
+    //   host: redisUrl.hostname,
+    //   port: Number(redisUrl.port),
+    //   ...(redisUrl.username && { username: redisUrl.username }),
+    //   ...(redisUrl.password && { password: redisUrl.password }),
       tls: {
         rejectUnauthorized: false,
       }, 
