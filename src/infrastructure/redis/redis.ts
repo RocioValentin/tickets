d4210344f7c875bc8b7f24b8e0b6f56c64dcc082
Rocio Valentin <rociovcl.10@gmail.com>
2026-03-23 00:43:13 -0500

    chore: changing env variables format in infrastructure  redis

--- src/infrastructure/redis/redis.ts
+++ src/infrastructure/redis/redis.ts
@@ -1,6 +1,11 @@
 import  Redis  from 'ioredis';
 
-export const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
+export const redis = new Redis(process.env.REDIS_URL!, {
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
+// export const redis = new Redis({
+//   host: process.env.REDIS_HOST,
+//   port: Number(process.env.REDIS_PORT),
+// });
